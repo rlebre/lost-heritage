9c319400f0683bf75945c1d8f32e98f1b2cf266d
Rui Lebre <ruilebre@ua.pt>
2020-09-28 14:02:20 +0100

    Added vue2-google-maps loading

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { route } from 'quasar/wrappers';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import * as VueGoogleMaps from 'vue2-google-maps';
+
 
 /*
  * If not building with SSR mode, you can
@@ -9,6 +11,12 @@ import routes from './routes';
 
 export default route(function ({ Vue }) {
   Vue.use(VueRouter);
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyCyCLaxQj1be6nNe5ii559A1DY-cqM3Z8s',
+      installComponents: true
+    }
+  });
 
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
