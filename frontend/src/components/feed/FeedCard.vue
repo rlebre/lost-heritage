e3b706e73be38a12030d89e9570fe9d10bb5b91d
Rui Lebre <ruilebre@ua.pt>
2020-10-22 15:58:59 +0100

    Added feedCard component; Changed FeedCard layout; Added hearts and comment layout

---
+++ frontend/src/components/feed/FeedCard.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-card class="card-post q-mb-md" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs q-pb-xs">
+        <div class="text-h5 q-mt-sm text-weight-bold">
+          {{ post.title }}
+        </div>
+      </q-card-section>
+    </q-card-section>
+
+    <div class="row q-mt-sm q-mx-md">
+      <div class="col-6">
+        <div class="text-subtitle2">
+          <q-icon name="eva-heart" color="negative" /> 1000
+        </div>
+      </div>
+      <div class="col-6">
+        <q-btn
+          class="q-mr-xs float-right"
+          round
+          color="primary"
+          icon="eva-facebook-outline"
+          size="xs"
+          unelevated
+        />
+
+        <q-btn
+          class="q-mr-xs float-right"
+          round
+          color="primary"
+          icon="eva-twitter-outline"
+          size="xs"
+          unelevated
+        />
+
+        <q-btn
+          class="q-mr-xs float-right"
+          round
+          color="primary"
+          icon="eva-share-outline"
+          size="xs"
+          unelevated
+        />
+      </div>
+    </div>
+
+    <q-card-section horizontal>
+      <q-card-section class="q-pt-xs col-7 q-mt-sm">
+        <div class="text-caption text-grey-7 small-screen-only">
+          {{ post.details | str_limit(200) }}
+        </div>
+        <div class="text-caption text-grey-7 large-screen-only">
+          {{ post.details }}
+        </div>
+      </q-card-section>
+
+      <q-card-section class="col-5 flex flex-center">
+        <q-img
+          class="rounded-borders"
+          :src="post.imageUrls[0]"
+          style="height:100px"
+        />
+      </q-card-section>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions>
+      <div class="col-1 q-mr-sm">
+        <q-btn
+          class="q-ml-sm"
+          round
+          color="negative"
+          icon="eva-heart"
+          size="sm"
+          unelevated
+        />
+      </div>
+      <div class="col-10 q-ml-sm">
+        <q-input
+          outlined
+          filled
+          placeholder="Comment"
+          dense
+          autogrow
+          v-model="newComment"
+        >
+          <template v-slot:after>
+            <q-btn
+              flat
+              rounded
+              color="primary large-screen-only"
+              label="Add Comment"
+            />
+            <q-btn flat rounded color="primary small-screen-only" icon="send" />
+          </template>
+        </q-input>
+      </div>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'FeedCard',
+
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      newComment: ''
+    };
+  }
+};
+</script>
--- frontend/src/pages/PageHome.vue
+++ frontend/src/pages/PageHome.vue
@@ -1,51 +1,16 @@
 <template>
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
-      <template v-if="!isLoadingPosts && postList.length">
-        <div class="col-12" v-for="post in postList" :key="post.id">
-          <template>
-            <q-card class="card-post q-mb-md" flat bordered>
-              <q-card-section horizontal>
-                <q-card-section class="q-pt-xs col-7">
-                  <div class="text-h5 q-mt-sm q-mb-lg text-weight-bold">
-                    {{ post.title }}
-                  </div>
-                  <div class="text-caption text-grey-7 small-screen-only">
-                    {{ post.details | str_limit(200) }}
-                  </div>
-                  <div class="text-caption text-grey-7 large-screen-only">
-                    {{ post.details }}
-                  </div>
-                </q-card-section>
-
-                <q-card-section class="col-5 flex flex-center">
-                  <q-img
-                    class="rounded-borders"
-                    :src="post.imageUrls[0]"
-                    style="height:100px"
-                  />
-                </q-card-section>
-              </q-card-section>
-
-              <!-- 
-              <q-separator />
-              
-              <q-card-actions>
-                <q-btn flat round icon="event" />
-                <q-btn flat>
-                  7:30PM
-                </q-btn>
-                <q-btn flat color="primary">
-                  Reserve
-                </q-btn>
-              </q-card-actions> -->
-            </q-card>
-          </template>
+      <template v-if="!isLoadingPosts && posts.length">
+        <div class="col-12" v-for="post in posts" :key="post.id">
+          <FeedCard :post="post"></FeedCard>
         </div>
       </template>
-      <div class="col-12" v-else-if="!isLoadingPosts && !postList.length">
+
+      <div class="col-12" v-else-if="!isLoadingPosts && !posts.length">
         <h5 class="text-center text-grey">No posts yet.</h5>
       </div>
+
       <div class="col-12" v-else>
         <q-card flat bordered>
           <q-card-section>
@@ -61,22 +26,6 @@
           <q-skeleton class="q-ma-sm" height="200px" animation="fade" />
         </q-card>
       </div>
-      <!-- <div class="col-4 large-screen-only">
-        <q-item class="fixed">
-          <q-item-section avatar>
-            <q-avatar size="48px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Rui Lebre</q-item-label>
-            <q-item-label caption>
-              @ruilebre
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </div> -->
     </div>
   </q-page>
 </template>
@@ -93,13 +42,26 @@ export default {
   },
 
   data() {
-    return {};
+    return {
+      posts: [],
+      activeComment: ''
+    };
   },
 
   filters: {
     niceDate(value) {
       return date.formatDate(value, 'MMMM D, HH:mm');
     }
+  },
+
+  watch: {
+    postList(newPosts) {
+      this.posts = newPosts;
+    }
+  },
+
+  created() {
+    this.posts = this.postList;
   }
 };
 </script>
