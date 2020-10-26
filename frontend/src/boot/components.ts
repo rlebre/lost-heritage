80b31439f2c6c4728f760add25e7647695f49aa0
Rui Lebre <ruilebre@ua.pt>
2020-10-26 19:02:38 +0000

    Added location picker on map

--- frontend/src/boot/components.ts
+++ frontend/src/boot/components.ts
@@ -4,6 +4,7 @@ import FeedCard from 'components/feed/FeedCard.vue';
 import MapComponent from 'components/map/MapComponent.vue';
 import ListComponent from 'components/map/ListComponent.vue';
 import ListCardComponent from 'components/map/ListCardComponent.vue';
+import MapLocationPicker from 'components/newPost/MapLocationPicker.vue';
 
 
 // "async" is optional;
@@ -13,4 +14,5 @@ export default boot(({ Vue }) => {
   Vue.component('MapComponent', MapComponent)
   Vue.component('ListComponent', ListComponent)
   Vue.component('ListCardComponent', ListCardComponent)
+  Vue.component('MapLocationPicker', MapLocationPicker)
 })
