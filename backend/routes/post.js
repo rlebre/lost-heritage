c07801c32b4a8e8e469b79cf01234d1324495ff8
Rui Lebre <ruilebre@ua.pt>
2020-11-07 22:53:23 +0000

    Added post comments; Added post likes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -7,7 +7,8 @@ router.post("/createPost", PostsCtrl.createPost);
 router.get("", PostsCtrl.getPublicPosts);
 router.get("/all", AuthMiddleware.authMiddleware, PostsCtrl.getAllPosts);
 router.get("/filtered", PostsCtrl.getFilteredPosts);
+router.post("/:id/like", PostsCtrl.likePost);
+router.post("/:id/comment", PostsCtrl.commentPost);
 router.get("/:id", PostsCtrl.getPostDetails);
-router.post("/like", PostsCtrl.likePost);
 
 module.exports = router;
