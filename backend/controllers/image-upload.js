6378b5392f798a4b12350a08a65ac199186bb951
Rui Lebre <ruilebre@ua.pt>
2020-11-07 16:33:18 +0000

    Added users services; added authentication

--- backend/controllers/image-upload.js
+++ backend/controllers/image-upload.js
@@ -1,12 +1,12 @@
 const upload = require('../services/image-upload');
-const singleUpload = upload.single('image');
+const arrayUpload = upload.array('image');
 
 exports.imageUpload = (req, res) => {
-    singleUpload(req, res, (err) => {
+    arrayUpload(req, res, (err) => {
         if (err) {
             return res.status(422).send({ errors: [{ title: "Image upload error.", detail: err.message }] });
         }
 
-        return res.json({ 'imageUrl': req.file.location });
+        return res.json({ 'imagesUrls': req.files.map(file => file.location) });
     })
 };
--- backend/routes/image-upload.js
+++ backend/routes/image-upload.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ImageUploadController = require("../controllers/image-upload");
 
-router.post("/image-upload", ImageUploadController.imageUpload);
+router.post("/upload", ImageUploadController.imageUpload);
 
 module.exports = router;
