71312de393bde56884c58a89e0670749cdbc42cc
Rui Lebre <ruilebre@ua.pt>
2020-11-11 10:45:45 +0000

    Applied filters to map

--- frontend/src/pages/PageMap.vue
+++ frontend/src/pages/PageMap.vue
@@ -7,50 +7,94 @@
     <div class="large-screen-only">
       <div class="app-panel row">
         <div class="list-panel col-sm-4 col-md-4">
-          <template v-if="posts.length">
-            <div class="col-12 q-ma-sm" v-for="post in posts" :key="post.id">
+          <template v-if="filteredPosts.length">
+            <div
+              class="col-12 q-ma-sm"
+              v-for="post in filteredPosts"
+              :key="post.id"
+            >
               <ListCardComponent :post="post"></ListCardComponent>
             </div>
           </template>
         </div>
         <div class="col-sm-8 col-md-8">
-          <MapComponent :posts="posts"></MapComponent>
+          <MapComponent :posts="filteredPosts"></MapComponent>
         </div>
       </div>
     </div>
 
     <div class="small-screen-only">
       <div class="app-panel row">
-        <MapComponent :posts="posts"></MapComponent>
+        <MapComponent :posts="filteredPosts"></MapComponent>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'PageMap',
 
   data() {
     return {
-      posts: []
+      filteredPosts: []
     };
   },
 
   computed: {
-    ...mapGetters('posts', ['postListWithClickField'])
-  },
-
-  methods: {
-    postFilterChanged(filterOptions) {
-      console.log(filterOptions);
-    }
+    ...mapGetters('posts', [
+      'postListWithClickField',
+      'isLoadingPosts',
+      'filteredPostList'
+    ])
   },
 
   created() {
-    this.posts = this.postListWithClickField;
+    this.filteredPosts = this.postListWithClickField;
+  },
+
+  watch: {
+    postListWithClickField(newValue, oldValue) {
+      this.filteredPosts = this.postListWithClickField;
+    },
+
+    filteredPostList(n, o) {
+      this.filteredPosts = this.filteredPostList;
+    }
+  },
+
+  methods: {
+    ...mapActions('posts', ['searchPosts']),
+
+    postFilterChanged(filterOptions) {
+      const { selectedOptions, sortBy, sortType, searchString } = filterOptions;
+
+      if (searchString) {
+        this.searchPosts({ query: searchString, sortBy, sortType });
+      } else {
+        this.filteredPosts = this.postListWithClickField;
+      }
+
+      if (selectedOptions.length > 0) {
+        this.filteredPosts = this.filteredPosts.filter(post => {
+          return selectedOptions.includes(post.county);
+        });
+      }
+
+      if (!searchString && selectedOptions.length === 0) {
+        this.filteredPosts = this.postListWithClickField;
+      }
+
+      if (sortBy) {
+        this.filteredPosts.sort(this.$dynamicSort(filterOptions.sortBy));
+      }
+
+      this.filteredPosts.sort(
+        this.$dynamicSort(filterOptions.sortBy, sortType === 'desc' ? -1 : 1)
+      );
+    }
   }
 };
 </script>
