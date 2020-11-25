bf4904be5c63a6ed2f10fad24b81148b912d3b62
Rui Lebre <ruilebre@ua.pt>
2020-11-25 20:26:00 +0000

    Updated page details

--- frontend/src/pages/PagePostDetail.vue
+++ frontend/src/pages/PagePostDetail.vue
@@ -1,49 +1,117 @@
 <template>
-  <q-page class="constrain q-pa-md">
-    <div class="col-12">
-      <h4 class="text-h4 text-bold q-ma-sm ">{{ post.title | capitalize }}</h4>
-    </div>
-    <div class="col-12">
-      <q-carousel
-        swipeable
-        arrows
-        animated
-        v-model="slideNumber"
-        thumbnails
-        infinite
-        :fullscreen.sync="fullscreen"
-      >
-        <q-carousel-slide
-          class="uncropped-image"
-          v-for="(image, index) in post.images"
-          :key="`blabla${index}`"
-          :name="index"
-          :img-src="image"
-        />
-        <template v-slot:control>
-          <q-carousel-control position="bottom-right" :offset="[18, 18]">
-            <q-btn
-              push
-              round
-              dense
-              color="white"
-              text-color="primary"
-              :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
-              @click="fullscreen = !fullscreen"
+  <q-page class="constrain-less q-pa-md">
+    <div class="upper-section">
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <q-carousel
+            swipeable
+            arrows
+            animated
+            v-model="slideNumber"
+            thumbnails
+            infinite
+            :fullscreen.sync="fullscreen"
+          >
+            <q-carousel-slide
+              class="uncropped-image"
+              v-for="(image, index) in post.images"
+              :key="`blabla${index}`"
+              :name="index"
+              :img-src="image"
             />
-          </q-carousel-control>
-        </template>
-      </q-carousel>
+            <template v-slot:control>
+              <q-carousel-control position="bottom-right" :offset="[18, 18]">
+                <q-btn
+                  push
+                  round
+                  dense
+                  color="white"
+                  text-color="primary"
+                  :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+                  @click="fullscreen = !fullscreen"
+                />
+              </q-carousel-control>
+            </template>
+          </q-carousel>
+        </div>
+
+        <div class="col-12 col-md-6">
+          <!-- <MapComponent style="max-width:100%" :posts="[post]"></MapComponent> -->
+          MAP
+        </div>
+      </div>
     </div>
 
-    <span>{{ post }}</span>
+    <div class="details-section">
+      <div class="details-section-header q-mb-md">
+        <div class="row">
+          <h3
+            :class="
+              `col-12 building-status building-${
+                post.isRecovered ? 'recovered' : 'not-recovered'
+              }`
+            "
+          >
+            {{ post.isRecovered ? 'Recovered' : 'Needs recovery' }}
+          </h3>
+          <h1 class="col-12 q-my-sm building-title">
+            {{ post.title | capitalize }}
+          </h1>
+          <h2 class="col-12 building-county">
+            {{ post.county | capitalize }}
+          </h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 col-md-8">
+          <hr class="q-mb-lg" />
+
+          <h2 class="building-details-title">Details</h2>
+          <p class="rental-description">
+            {{ post.details }}
+          </p>
+
+          <h2 class="building-details-title" v-if="post.stories">Stories</h2>
+          <p class="rental-description">
+            {{ post.stories }}
+          </p>
+
+          <h2 class="building-details-title" v-if="post.previousFunctions">
+            Previous functions
+          </h2>
+          <p class="rental-description">
+            {{ post.previousFunctions }}
+          </p>
+
+          <h2 class="building-details-title" v-if="post.suggestedFunctions">
+            Suggested functions
+          </h2>
+          <p class="rental-description">
+            {{ post.suggestedFunctions }}
+          </p>
+
+          <div class="rental-rating">
+            <span>2 / 5</span>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4">
+          olaaa
+        </div>
+      </div>
+    </div>
+
+    <!-- <span>{{ post }}</span> -->
   </q-page>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import MapComponent from '../components/map/MapComponent.vue';
 
 export default {
+  components: { MapComponent },
   name: 'PagePost',
 
   data() {
@@ -79,5 +147,35 @@ export default {
   background-size: contain; /* don't crop the image  */
   background-repeat: no-repeat; /* only show the image one time  */
   //background-color: grey; /* color to fill empty space with  */
+}
+
+.upper-section {
+  margin-bottom: 30px;
+}
+
+.building {
+  &-status {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 0;
+  }
+
+  &-title {
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 2rem;
+  }
+
+  &-county {
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 0;
+  }
+
+  &-details-title {
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 0;
+  }
 }
 </style>
