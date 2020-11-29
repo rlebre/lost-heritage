81559d6409bd7942e3eb1e820302f437bb016562
Rui Lebre <ruilebre@ua.pt>
2020-11-29 22:52:39 +0000

    Removed vue2-google-maps completely

--- frontend/src/boot/google-maps.ts
+++ frontend/src/boot/google-maps.ts
@@ -1,18 +1,9 @@
-const VueGoogleMaps2 = require('vue2-google-maps');
-
 import 'vue-googlemaps/dist/vue-googlemaps.css';
 import VueGoogleMaps from 'vue-googlemaps';
 
 import { boot } from 'quasar/wrappers';
 
 export default boot(({ Vue }) => {
-    Vue.use(VueGoogleMaps2, {
-        load: {
-            key: 'AIzaSyCyCLaxQj1be6nNe5ii559A1DY-cqM3Z8s',
-            installComponents: true
-        }
-    });
-
     Vue.use(VueGoogleMaps, {
         load: {
             // Google API key
