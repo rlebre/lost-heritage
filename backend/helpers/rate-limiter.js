5ca9843d6bad178d9b1504d13f41061add3419b3
Rui Lebre <ruilebre@ua.pt>
2020-12-01 18:19:55 +0000

    Implemented rate limit on create post and comment post

--- backend/helpers/rate-limiter.js
+++ backend/helpers/rate-limiter.js
@@ -2,12 +2,34 @@ const rateLimit = require("express-rate-limit");
 
 module.exports = {
     likeLimiter: rateLimit({
-        windowMs: 5 * 60 * 1000, // 10 minutes block
+        windowMs: 24 * 60 * 60 * 1000, // 1 day block
         max: 10, // start blocking after 5 requests
         message: {
             errors: [{
                 title: "Too many requests",
                 message: "Too many likes from this IP, please try again later."
+            }]
+        }
+    }),
+
+    createPostLimiter: rateLimit({
+        windowMs: 10 * 60 * 1000, // 10 minutes block
+        max: 5, // start blocking after 5 requests
+        message: {
+            errors: [{
+                title: "Too many requests",
+                message: "Too many create post from this IP, please try again later."
+            }]
+        }
+    }),
+
+    commentLimiter: rateLimit({
+        windowMs: 24 * 60 * 60 * 1000, // 1 day block
+        max: 3, // start blocking after 3 requests
+        message: {
+            errors: [{
+                title: "Too many requests",
+                message: "Too many comments from this IP, please try again later."
             }]
         }
     })
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const PostsCtrl = require("../controllers/posts");
 const AuthMiddleware = require("../middlewares/authMiddleware");
-const { likeLimiter } = require("../helpers/rate-limiter");
+const { likeLimiter, createPostLimiter, commentLimiter } = require("../helpers/rate-limiter");
 
-router.post("/createPost", PostsCtrl.createPost);
+router.post("/createPost", createPostLimiter, PostsCtrl.createPost);
 router.get("/filter", PostsCtrl.filterPosts);
 router.get("/all", AuthMiddleware.authMiddleware, PostsCtrl.getAllPosts);
 router.get("/filtered", PostsCtrl.getFilteredPosts);
 router.get("/search", PostsCtrl.searchPosts);
 router.get("", PostsCtrl.getPublicPosts);
 router.post("/:id/like", likeLimiter, PostsCtrl.likePost);
-router.post("/:id/comment", PostsCtrl.commentPost);
+router.post("/:id/comment", commentLimiter, PostsCtrl.commentPost);
 router.get("/:id", PostsCtrl.getPostDetails);
 
 module.exports = router;
