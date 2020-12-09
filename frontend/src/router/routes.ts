fd4027c7bbe5c0f68eb38cd3ee6334b6de4794a8
Rui Lebre <ruilebre@ua.pt>
2020-12-09 18:57:06 +0000

    Added new admin routes

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -11,13 +11,6 @@ const routes: RouteConfig[] = [
       { path: '/map', component: PageMap },
       { path: '/new-post', component: () => import('pages/PageCreatePost.vue') },
       { path: '/post/:id', component: () => import('pages/PagePostDetail.vue') },
-      {
-        path: '/admin',
-        component: () => import('pages/PageAdmin.vue'),
-        meta: {
-          requiresAuth: true
-        }
-      }
     ]
   },
   {
@@ -27,6 +20,34 @@ const routes: RouteConfig[] = [
       { path: '', component: () => import('pages/PageAuth.vue') }
     ]
   },
+  {
+    path: '/admin',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/PageAdmin.vue'),
+        meta: {
+          requiresAuth: true
+        }
+      },
+      {
+        path: 'pending-posts',
+        component: () => import('pages/PageAdmin.vue'),
+        meta: {
+          requiresAuth: true
+        }
+      },
+      {
+        path: 'pending-comments',
+        component: () => import('pages/PageAdmin.vue'),
+        meta: {
+          requiresAuth: true
+        }
+      }
+    ]
+  },
+
 
   // Always leave this as last one,
   // but you can also remove it
