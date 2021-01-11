7c41004fcbedc7bf1081ea9924bfca48ee68e28f
Rui Lebre <ruilebre@ua.pt>
2021-01-11 15:13:57 +0000

    Created Approved and Declined posts tables

--- frontend/src/pages/admin/ApprovedPosts.vue
+++ frontend/src/pages/admin/ApprovedPosts.vue
@@ -2,50 +2,10 @@
   <q-page class="q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12">
-        <template v-if="posts">
-          <q-table
-            :data="posts"
-            :columns="columns"
-            row-key="index"
-            :pagination.sync="pagination"
-          >
-            <template v-slot:body-cell-preview="props">
-              <q-td :props="props">
-                <router-link class="no-link" :to="`post/${props.row._id}`">
-                  <q-btn dense round flat color="primary" icon="eva-search">
-                    <q-tooltip>Preview post</q-tooltip>
-                  </q-btn>
-                </router-link>
-              </q-td>
-            </template>
-
-            <template v-slot:body-cell-actions="props">
-              <q-td :props="props">
-                <q-btn
-                  dense
-                  round
-                  flat
-                  color="warning"
-                  icon="eva-edit"
-                  disabled
-                >
-                  <q-tooltip>Edit post</q-tooltip>
-                </q-btn>
-
-                <q-btn
-                  dense
-                  round
-                  flat
-                  color="negative"
-                  icon="eva-close-circle"
-                  @click="onRemovePostClick(props.row)"
-                >
-                  <q-tooltip>Remove post</q-tooltip>
-                </q-btn>
-              </q-td>
-            </template>
-          </q-table>
-        </template>
+        <ApprovedPostsTable
+          :posts="posts"
+          @removePost="onRemovePostClick"
+        ></ApprovedPostsTable>
       </div>
     </div>
   </q-page>
@@ -59,59 +19,6 @@ export default {
 
   data() {
     return {
-      pagination: {
-        rowsPerPage: 10
-      },
-      columns: [
-        {
-          name: 'preview',
-          label: 'Preview',
-          field: '',
-          align: 'center',
-          style: 'max-width: 1%; white-space: initial'
-        },
-        {
-          name: 'title',
-          label: 'Title',
-          field: 'title',
-          align: 'left',
-          sortable: true,
-          style: 'max-width: 10%; white-space: initial'
-        },
-        {
-          name: 'details',
-          label: 'Details',
-          field: 'details',
-          sortable: true,
-          align: 'left',
-          style: 'max-width: 20%; white-space: initial'
-        },
-        {
-          name: 'county',
-          label: 'County',
-          field: 'county',
-          align: 'left',
-          sortable: true,
-          style: 'max-width: 60%; white-space: initial'
-        },
-        {
-          name: 'approvedBy',
-          label: 'Approved By',
-          field: 'approvedBy',
-          align: 'left',
-          sortable: true,
-          style: 'max-width: 60%; white-space: initial'
-        },
-        {
-          name: 'approvedAt',
-          label: 'Published At',
-          field: 'approvedAt',
-          align: 'left',
-          sortable: true,
-          style: 'max-width: 60%; white-space: initial'
-        },
-        { name: 'actions', label: 'Actions', field: '', align: 'center' }
-      ],
       posts: []
     };
   },
