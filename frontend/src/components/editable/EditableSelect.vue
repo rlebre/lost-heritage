400c36727d9ae5d428a54e2687d26e638f2c2cd3
Rui Lebre <ruilebre@ua.pt>
2021-01-14 23:04:03 +0000

    Activated editable features

--- frontend/src/components/editable/EditableSelect.vue
+++ frontend/src/components/editable/EditableSelect.vue
@@ -1,30 +1,43 @@
 <template>
-  <q-btn-toggle
-    v-model="inputData"
-    spread
-    no-caps
-    :class="
-      `col-12 q-my-sm building-status building-${
-        inputData ? 'recovered' : 'not-recovered'
-      }`
-    "
-    unelevated
-    toggle-color="primary"
-    color="grey-4"
-    text-color="grey-9"
-    :options="[
-      { label: 'Recovered', value: true },
-      { label: 'Needs recovery', value: false }
-    ]"
-    @input="inputChanged"
-  />
+  <div>
+    <q-btn-toggle
+      v-if="editable"
+      v-model="inputData"
+      spread
+      no-caps
+      :class="
+        `col-12 q-my-sm building-status building-${
+          inputData ? 'recovered' : 'not-recovered'
+        }`
+      "
+      unelevated
+      toggle-color="primary"
+      color="grey-4"
+      text-color="grey-9"
+      :options="[
+        { label: 'Recovered', value: true },
+        { label: 'Needs recovery', value: false }
+      ]"
+      @input="inputChanged"
+    />
+    <h3
+      v-else
+      :class="
+        `col-12 building-status building-${
+          this.default ? 'recovered' : 'not-recovered'
+        }`
+      "
+    >
+      {{ this.default ? 'Recovered' : 'Needs recovery' }}
+    </h3>
+  </div>
 </template>
 
 <script>
 import { debounce } from 'quasar';
 
 export default {
-  name: 'EditableSelect',
+  name: 'EditableToggle',
 
   props: {
     default: {
@@ -43,6 +56,10 @@ export default {
     },
     inputClass: {
       type: String
+    },
+    editable: {
+      type: Boolean,
+      required: true
     }
   },
 
@@ -64,3 +81,13 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.building {
+  &-status {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 0;
+  }
+}
+</style>
