1a746182d9025714349318ed7c27a7c950d4de9a
Rui Lebre <ruilebre@ua.pt>
2021-01-30 20:33:20 +0000

    Added PT-PT translations; Added traslations to filter

--- frontend/src/i18n/pt-pt/index.ts
+++ frontend/src/i18n/pt-pt/index.ts
@@ -1,5 +1,185 @@
+// This is just an example,
+// so you can safely delete all default props below
+
 export default {
     header: {
-        appName: 'Património Esquecido'
+        appName: 'Lost Heritage'
+    },
+
+    c: {
+        filter: {
+            filters: 'filtros',
+            counties: 'concelhos',
+            county: 'concelho',
+            countyHint: 'Max 5 seleções',
+            date: 'date',
+            sort: 'ordenar',
+            search: 'pesquisar',
+            likes: 'gostos',
+            like: 'gosto',
+            applyFilters: 'aplicar filtros',
+            apply: 'aplicar',
+            reset: 'reiniciar'
+        },
+
+        settings: {
+            settings: 'settings',
+            nightMode: 'night mode',
+            language: 'language',
+            english: 'english',
+            portuguese: 'portuguese',
+            instructions: 'instructions',
+            about: 'about',
+            tc: 'terms and conditions',
+            show: 'show',
+        },
+
+        card: {
+            comment: 'comment',
+            addComment: 'add comment'
+        },
+
+        commentBox: {
+            comments: 'comments',
+            createComment: 'create new comment'
+        }
+    },
+
+    create: {
+        createNewPost: 'create new post',
+        contributorInfo: 'contributor information',
+        contributorName: 'contributor name',
+        contributorEmail: 'contributor email',
+        contributorCity: 'contributor city',
+        buildingInfo: 'building information',
+        buildTitle: 'title',
+        buildDetails: 'details',
+        buildCounty: 'county',
+        buildLocation: 'Pick or Locate Below',
+        pick: 'pick',
+        locate: 'locate',
+        buildPrevFunctions: 'building previous functions',
+        buildSugestFunctions: 'building suggested functions',
+        recovered: 'recovered',
+        needsRecovery: 'needs recovery',
+        chooseImage: 'choose an image',
+        newsletter: 'I want to receive e-mails with news.',
+        agreeTC1: 'I agree with the',
+        agreeTC2: ', recognizing that the personal data will be stored for validation and possible future contact.',
+        submit: 'submit'
+    },
+
+    details: {
+        recovered: 'recovered',
+        needsRecovery: 'needs recovery',
+        details: 'details',
+    },
+
+    instructions: {
+        wantSubmit: 'Want to submit?',
+        wantTopics: [
+            'Go to Create Post page',
+            'Fill in at least the required fields',
+            'If you have, upload some images of the building',
+            'Accept our terms and conditions, if you agree with them',
+            'Submit and... it\'s done!'
+        ],
+        want1: 'Now, our curators will look into your post. If the building was already posted, don\'t worry, we will look into it and update with the awesome informations you sent.',
+        want2: 'If everything fits our T&C, your post will be available for everyone soon! Bear in mind that posts containing racism, nudity or some sort of discrimination will not be tolerated and the submitter will be banned.',
+
+        stillSearching: 'Still searching for help?',
+        still1: 'Need more help with the platform? Found a bug? Want to contribute with you programming skills? Have a suggestion? E-mail us:'
+    },
+
+    about: {
+        paragraphs: [
+            'Lost Heritage is a non-profit project. The main goal of the project is to map and gather information about abandoned public buildings in Portugal.',
+
+            'Once the information about the buildings is centralized, it can be used to develop plans and public discussion about the recovering of such legacy.',
+
+            'In fact, there are many good examples in the field. Our platform maps also heritage which was once forgotten but nowadays has gained a new purpose.',
+
+            'Feel free to contribute! Lost Heritage platform is open to any citizen of the world, because there are no boundaries and we all are seeking for a common goal. The maintainers of the project thank for your contribution. However, please bear in mind that for quality purposes, the contributions will be curated and may be edited before approval by our curators.'
+        ],
+        checknews: 'Check out the news:',
+        follow: 'Follow us on social media:',
+        contribute: 'Contribute for this open-source platform:'
+    },
+
+    termsandconditions: {
+        terms: `Welcome to Lost Heritage!
+  
+      These terms and conditions outline the rules and regulations for the use of Lost Heritage's Website.
+      
+      By accessing this website we assume you accept these terms and conditions.Do not continue to use Lost Heritage if you do not agree to take all of the terms and conditions stated on this page.
+      
+      The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves.All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Portugal.Any use of the above terminology or other words in the singular, plural, capitalization and / or he / she or they, are taken as interchangeable and therefore as referring to same.
+      
+          Cookies
+      We employ the use of cookies.By accessing Lost Heritage, you agreed to use cookies in agreement with the Lost Heritage's Privacy Policy.
+      
+      Most interactive websites use cookies to let us retrieve the user’s details for each visit.Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website.Some of our affiliate / advertising partners may also use cookies.We also use cookies to track the number of our visitors.
+      
+          License
+      Unless otherwise stated, Lost Heritage and / or its licensors own the intellectual property rights for all material on Lost Heritage.All intellectual property rights are reserved.You may access this from Lost Heritage for your own personal use subjected to restrictions set in these terms and conditions.
+      
+      You must not:
+      
+      Republish material from Lost Heritage
+      Sell, rent or sub - license material from Lost Heritage
+      Reproduce, duplicate or copy material from Lost Heritage
+      Redistribute content from Lost Heritage
+      This Agreement shall begin on the date hereof.Our Terms and Conditions were created with the help of the Terms And Conditions Generator and the Privacy Policy Generator.
+      
+      Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website.Lost Heritage does not filter, edit, publish or review Comments prior to their presence on the website.Comments do not reflect the views and opinions of Lost Heritage, its agents and / or affiliates.Comments reflect the views and opinions of the person who post their views and opinions.To the extent permitted by applicable laws, Lost Heritage shall not be liable for the Comments or for any liability, damages or expenses caused and / or suffered as a result of any use of and / or posting of and / or appearance of the Comments on this website.
+      
+      Lost Heritage reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
+      
+      You warrant and represent that:
+      
+      You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;
+      The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;
+      The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy
+      The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.
+      You hereby grant Lost Heritage a non - exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
+      
+      Hyperlinking to our Content
+      The following organizations may link to our Website without prior written approval:
+      
+      Government agencies;
+      Search engines;
+      News organizations;
+      Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and
+      System wide Accredited Businesses except soliciting non - profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.
+      These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and / or services; and(c) fits within the context of the linking party’s site.
+      
+      Content Liability
+      We shall not be hold responsible for any content that appears on your Website.You agree to protect and defend us against all claims that is rising on your Website.No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
+      
+      Your Privacy
+      Please read Privacy Policy
+      
+      Reservation of Rights
+      We reserve the right to request that you remove all links or any particular link to our Website.You approve to immediately remove all links to our Website upon request.We also reserve the right to amen these terms and conditions and it’s linking policy at any time.By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.
+      
+      Removal of links from our website
+      If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment.We will consider requests to remove links but we are not obligated to or so or to respond to you directly.
+      
+      We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+      
+          Disclaimer
+      To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website.Nothing in this disclaimer will:
+      
+      limit or exclude our or your liability for death or personal injury;
+      limit or exclude our or your liability for fraud or fraudulent misrepresentation;
+      limit any of our or your liabilities in any way that is not permitted under applicable law; or
+      exclude any of our or your liabilities that may not be excluded under applicable law.
+      The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and(b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.
+      
+      As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.`
+    },
+
+    error: {
+        message: 'Oops. Nothing here...'
     }
 };
