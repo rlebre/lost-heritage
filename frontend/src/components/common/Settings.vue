e4c2b13a9cbea7c04a084e9750c41340bc40070a
Rui Lebre <ruilebre@ua.pt>
2021-01-31 00:19:45 +0000

    Added t&c to translate document; Settings changed to have translations; Terms changed to have translation

--- frontend/src/components/common/Settings.vue
+++ frontend/src/components/common/Settings.vue
@@ -3,12 +3,16 @@
     <q-dialog v-model="sliders" @hide="onClose">
       <q-card style="width: 300px" class="q-px-sm q-pb-md">
         <q-card-section class="row items-center q-pb-none">
-          <div class="text-h6">Settings</div>
+          <div class="text-h6 text-capitalize">
+            {{ $t('c.settings.settings') }}
+          </div>
           <q-space />
           <q-btn icon="close" flat round dense v-close-popup />
         </q-card-section>
 
-        <q-item-label header>Night Mode</q-item-label>
+        <q-item-label header class="text-capitalize">
+          {{ $t('c.settings.nightMode') }}
+        </q-item-label>
         <q-item dense>
           <q-item-section avatar>
             <q-icon name="eva-moon" />
@@ -24,7 +28,9 @@
           </q-item-section>
         </q-item>
 
-        <q-item-label header>Language</q-item-label>
+        <q-item-label header class="text-capitalize">
+          {{ $t('c.settings.language') }}
+        </q-item-label>
         <q-item dense>
           <q-item-section avatar>
             <q-icon name="eva-globe" />
@@ -41,33 +47,54 @@
           </q-item-section>
         </q-item>
 
-        <q-item-label header>Instructions</q-item-label>
+        <q-item-label header class="text-capitalize">
+          {{ $t('c.settings.instructions') }}
+        </q-item-label>
         <q-item dense>
           <q-item-section avatar>
             <q-icon name="eva-question-mark-circle" />
           </q-item-section>
           <q-item-section>
-            <q-btn dense label="Show" @click="instructionsModal = true" />
+            <q-btn
+              dense
+              class="text-capitalize"
+              :label="$t('c.settings.show')"
+              @click="instructionsModal = true"
+            />
           </q-item-section>
         </q-item>
 
-        <q-item-label header>About</q-item-label>
+        <q-item-label header class="text-capitalize">
+          {{ $t('c.settings.about') }}
+        </q-item-label>
         <q-item dense>
           <q-item-section avatar>
             <q-icon name="eva-info" />
           </q-item-section>
           <q-item-section>
-            <q-btn dense label="Show" @click="aboutModal = true" />
+            <q-btn
+              dense
+              class="text-capitalize"
+              :label="$t('c.settings.show')"
+              @click="aboutModal = true"
+            />
           </q-item-section>
         </q-item>
 
-        <q-item-label header>Terms & Conditions</q-item-label>
+        <q-item-label header class="text-capitalize">
+          {{ $t('c.settings.tc') }}
+        </q-item-label>
         <q-item dense>
           <q-item-section avatar>
             <q-icon name="eva-book" />
           </q-item-section>
           <q-item-section>
-            <q-btn dense label="Show" @click="tcModal = true" />
+            <q-btn
+              dense
+              class="text-capitalize"
+              :label="$t('c.settings.show')"
+              @click="tcModal = true"
+            />
           </q-item-section>
         </q-item>
       </q-card>
@@ -80,18 +107,20 @@
             <img src="lost-heritage.png" />
           </q-avatar>
 
-          <q-toolbar-title class="text-weight-bold">
-            Instructions
+          <q-toolbar-title class="text-weight-bold text-capitalize">
+            {{ $t('c.settings.instructions') }}
           </q-toolbar-title>
 
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
 
         <q-card-section class="q-pa-sm">
-          <div class="text-h6">Want to submit?</div>
+          <div class="text-h6">
+            {{ $t('instructions.wantSubmit') }}
+          </div>
           <ol class="q-mt-auto q-mb-sm">
             <li>
-              Go to Create Post page (<q-btn
+              {{ $t('instructions.wantTopics[0]') }} (<q-btn
                 flat
                 round
                 icon="eva-plus-circle-outline"
@@ -100,30 +129,28 @@
                 to="/new-post"
               />)
             </li>
-            <li>Fill in at least the required fields</li>
-            <li>If you have, upload some images of the building</li>
-            <li>Accept our terms and conditions, if you agree with them</li>
-            <li>Submit and... it's done!</li>
+            <li
+              v-for="(entry, index) in $t('instructions.wantTopics').slice(1)"
+              :key="index"
+            >
+              {{ entry }}
+            </li>
           </ol>
 
           <p>
-            Now, our curators will look into your post. If the building was
-            already posted, don't worry, we will look into it and update with
-            the awesome informations you sent.
+            {{ $t('instructions.want1') }}
           </p>
 
           <p>
-            If everything fits our T&C, your post will be available for everyone
-            soon! Bear in mind that posts containing racism, nudity or some sort
-            of discrimination will not be tolerated and the submitter will be
-            banned.
+            {{ $t('instructions.want2') }}
           </p>
 
-          <div class="text-h6">Still searching for help?</div>
+          <div class="text-h6">{{ $t('instructions.stillSearching') }}</div>
           <p>
-            Need more help with the platform? Found a bug? Want to contribute
-            with you programming skills? Have a suggestion? E-mail us:
-            <a href="mailto:ruilebre@ua.pt">ruilebre@ua.pt</a>
+            {{ $t('instructions.still1') }}
+            <a href="mailto:patrimonioesquecido2020@gmail.com">
+              patrimonioesquecido2020@gmail.com
+            </a>
           </p>
         </q-card-section>
       </q-card>
@@ -143,36 +170,16 @@
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
 
-        <q-card-section>
-          Lost Heritage is a non-profit project. The main goal of the project is
-          to map and gather information about abandoned public buildings in
-          Portugal.
-        </q-card-section>
-
-        <q-card-section>
-          Once the information about the buildings is centralized, it can be
-          used to develop plans and public discussion about the recovering of
-          such legacy.
-        </q-card-section>
-
-        <q-card-section>
-          In fact, there are many good examples in the field. Our platform maps
-          also heritage which was once forgotten but nowadays has gained a new
-          purpose.
-        </q-card-section>
-
-        <q-card-section>
-          Feel free to contribute! Lost Heritage platform is open to any citizen
-          of the world, because there are no boundaries and we all are seeking
-          for a common goal. The maintainers of the project thank for your
-          contribution. However, please bear in mind that for quality purposes,
-          the contributions will be curated and may be edited before approval by
-          our curators.
+        <q-card-section
+          v-for="(paragraph, index) in $t('about.paragraphs')"
+          :key="index"
+        >
+          {{ paragraph }}
         </q-card-section>
 
         <q-card-section>
           <p>
-            Check out the news:
+            {{ $t('about.checknews') }}
             <a
               href="https://www.publico.pt/2020/09/20/local/noticia/portugueses-desafiados-descobrir-patrimonio-esquecido-1932046"
               >in Publico by Maria José Santana</a
@@ -180,12 +187,12 @@
           </p>
 
           <p>
-            Follow us on social media:
+            {{ $t('about.follow') }}
             <a href="https://www.facebook.com/patrimonioesquecido">Facebook</a>
           </p>
 
           <p>
-            Contribute for this open-source platform:
+            {{ $t('about.contribute') }}
             <a href="https://github.com/rlebre/lost-heritage">GitHub</a>
           </p>
         </q-card-section>
