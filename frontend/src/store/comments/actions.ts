2460c4fdd8bcda6a92a5f7ecb36b0572f11c5fe6
Rui Lebre <ruilebre@ua.pt>
2021-02-02 10:09:26 +0000

    Every action now returns a Promise

--- frontend/src/store/comments/actions.ts
+++ frontend/src/store/comments/actions.ts
@@ -1,12 +1,14 @@
 export function likeComment(this: any, { commit }: any, commentd: string) {
     commit('likeCommentRequest');
 
-    this.$axios
+    return this.$axios
         .post(`${process.env.API}/api/v1/comments/${commentd}/like`)
         .then((response: any) => {
             commit('likeCommentSuccess', response.data);
+            return Promise.resolve(response.data);
         })
         .catch((error: any) => {
             commit('likeCommentFailure', error.response.data);
+            return Promise.reject(error.response.data.errors);
         })
 }
