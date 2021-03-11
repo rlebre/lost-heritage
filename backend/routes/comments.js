266d9c8762d5e8577727c1484452aafb351933b9
Rui Lebre <ruilebre@ua.pt>
2021-03-11 15:09:29 +0000

    Removed backend (available in new repo)

--- backend/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CommentsCtrl = require("../controllers/comments");
-const { likeLimiter } = require("../helpers/rate-limiter");
-
-router.post("/:id/like", likeLimiter, CommentsCtrl.likeComment);
-
-module.exports = router;
--- backend/routes/info.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const InfosCtrl = require("../controllers/infos");
-
-router.get("/existing-counties", InfosCtrl.existingCounties);
-
-module.exports = router;
