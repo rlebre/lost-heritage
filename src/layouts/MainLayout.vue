3f1e43a8456ee77de0c78b4bca1932b278c80a46
Rui Lebre <ruilebre@ua.pt>
2021-04-01 08:53:24 +0100

    Replaced title - now matches locale

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,33 +4,25 @@
       <q-toolbar class="constrain">
         <!-- <q-separator vertical spaced class="large-screen-only" /> -->
 
-        <router-link class="no-link" to="/">
-          <q-toolbar-title class="text-grand-hotel text-bold toolbar-title">
+        <router-link class="no-link flex" to="/">
+          <q-avatar class="q-my-auto large-screen-only">
+            <img src="lost-heritage.png" />
+          </q-avatar>
+
+          <q-avatar class="q-my-auto small-screen-only absolute-center">
+            <img src="lost-heritage-full.png" />
+          </q-avatar>
+
+          <q-toolbar-title class="large-screen-only text-grand-hotel text-bold toolbar-title">
             {{ $t('appName') }}
           </q-toolbar-title>
         </router-link>
 
         <div class="q-space"></div>
 
-        <q-btn
-          flat
-          round
-          icon="eva-home-outline"
-          size="18px"
-          dense
-          to="/"
-          class="large-screen-only q-mr-lg"
-        />
+        <q-btn flat round icon="eva-home-outline" size="18px" dense to="/" class="large-screen-only q-mr-lg" />
 
-        <q-btn
-          flat
-          round
-          icon="eva-map-outline"
-          size="18px"
-          dense
-          to="/map"
-          class="large-screen-only q-mr-lg"
-        />
+        <q-btn flat round icon="eva-map-outline" size="18px" dense to="/map" class="large-screen-only q-mr-lg" />
 
         <q-btn
           flat
@@ -42,32 +34,17 @@
           class="large-screen-only q-mr-lg"
         />
 
-        <q-btn
-          flat
-          round
-          icon="eva-settings-2-outline"
-          size="18px"
-          dense
-          @click="showSettingsDialog = true"
-        />
+        <q-btn flat round icon="eva-settings-2-outline" size="18px" dense @click="showSettingsDialog = true" />
 
         <Settings :show="showSettingsDialog" @onClose="settingsClosed" />
       </q-toolbar>
     </q-header>
 
     <q-footer class="small-screen-only q-footer" bordered>
-      <q-tabs
-        class="text-grey-10"
-        active-color="primary"
-        indicator-color="transparent"
-      >
+      <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
         <q-route-tab to="/" icon="eva-home-outline" class="q-route-tab" />
         <q-route-tab to="/map" icon="eva-map-outline" class="q-route-tab" />
-        <q-route-tab
-          to="/new-post"
-          icon="eva-plus-circle-outline"
-          class="q-route-tab"
-        />
+        <q-route-tab to="/new-post" icon="eva-plus-circle-outline" class="q-route-tab" />
       </q-tabs>
     </q-footer>
 
@@ -97,6 +74,12 @@ export default {
     settingsClosed() {
       this.showSettingsDialog = false;
     }
+  },
+
+  meta() {
+    return {
+      title: this.$i18n.t('appName')
+    };
   }
 };
 </script>
